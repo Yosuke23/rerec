378faad7c5cf276af69b02140c731cda134a79eb
“yosuke” <yosuke4123@gmail.com>
2020-05-26 19:10:14 +0900

    UsersテーブルにAvatarカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_063550) do
+ActiveRecord::Schema.define(version: 2020_05_26_100623) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "googlebooksapi_id", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_063550) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
